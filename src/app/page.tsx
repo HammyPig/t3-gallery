80a2f784432de9a64baba660df96f34f05e2c027
HammyPig <james.bruce.walden@gmail.com>
2025-10-03 12:43:43 +1000

    feat: replace gallery text placeholders with images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { desc } from "drizzle-orm";
 import Link from "next/link";
 import { db } from "~/server/db";
 
@@ -15,14 +16,17 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="w-48">
-            {post.name}
+        {images.map((image) => (
+          <div key={image.id} className="w-48 flex flex-col">
+            <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
