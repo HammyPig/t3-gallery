b574ac15d840cc574f8af66dbcc5fd92c89561a0
HammyPig <james.bruce.walden@gmail.com>
2025-10-03 11:46:20 +1000

    feat: add gallery scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://dummyimage.com/600x400/ff0000/ffffff.jpg&text=Image+1",
+  "https://dummyimage.com/600x400/00ff00/ffffff.jpg&text=Image+2",
+  "https://dummyimage.com/600x400/0000ff/ffffff.jpg&text=Image+3"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}));
+
 export default function HomePage() {
   return (
     <main>
-      Hello, World!
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
